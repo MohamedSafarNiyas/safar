47395e61521a1fa13dd17dd3ce26e51d1b18e37b
MohamedSafarNiyas <112833877+MohamedSafarNiyas@users.noreply.github.com>
2023-10-26 15:23:22 +0530

    Update settings.py

--- devopsproject/settings.py
+++ devopsproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r+fnc4k*crv!qvf76ekmoxlz%x5*cc7b*!(1c!)^#)$bs0db0i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
